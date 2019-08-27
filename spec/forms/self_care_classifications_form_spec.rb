77bb7f9fadff351d3cc86f603569a54537dbeea4
suzuki_mar <supasu145@gmail.com>
2019-08-27 10:20:03 +0900

    Rubocopで指摘された項目の修正

--- spec/forms/self_care_classifications_form_spec.rb
+++ spec/forms/self_care_classifications_form_spec.rb
@@ -2,12 +2,12 @@
 
 require 'rails_helper'
 
-describe SelfCareClassificationsForm, type: :model do
+describe SelfCareClassificationsForm, type: :form do
   let(:user) { create(:user) }
   let(:form) { described_class.new(user, params) }
 
   describe 'validate' do
-    subject { form.validate }
+    subject(:validate) { form.validate }
 
     context 'パラメーターが正しい場合' do
       let(:params) do
@@ -15,18 +15,15 @@ describe SelfCareClassificationsForm, type: :model do
           'good' => [
             { 'id' =>  '', 'name' => 'name1', 'order_number' => 1 }
           ],
-
           'normal' => [
             { 'id' =>  '', 'name' => 'name1', 'order_number' => 2 }
           ],
-
-          'bad' => [
-          ]
+          'bad' => []
         }
       end
 
       it 'バリデーションエラーがないこと' do
-        expect(subject).to be_truthy
+        is_expected.to be_truthy
       end
     end
 
@@ -42,15 +39,13 @@ describe SelfCareClassificationsForm, type: :model do
               { 'id' =>  '', 'name' => 'name1', 'order_number' => 2 }
             ],
 
-            'bad' => [
-            ],
-            'error' => [
-            ]
+            'bad' => [],
+            'error' => []
           }
         end
 
         it 'バリデーションエラーメッセージを取得できること' do
-          subject
+          validate
           expect(form.errors.messages[:kind_name]).to eq(['不正な項目名が渡されました:error'])
         end
       end
@@ -63,13 +58,13 @@ describe SelfCareClassificationsForm, type: :model do
             ],
 
             'normal' => [
-              { 'id' =>  '', 'name' =>  'name1',  'order_number' => 2 }
+              { 'id' =>  '', 'name' => 'name1', 'order_number' => 2 }
             ]
           }
         end
 
         it 'バリデーションエラーメッセージを取得できること' do
-          subject
+          validate
           expect(form.errors.messages[:kind_name]).to eq(['足りない項目名があります:bad'])
         end
       end
@@ -78,13 +73,15 @@ describe SelfCareClassificationsForm, type: :model do
         let(:params) do
           {
             'good' => [{ 'id' => '', 'name' => 'name1' }],
-            'normal' => [{ 'id' =>  '', 'order_number' => 2 }],
-            'bad' => [{ 'id' => '', 'name' => 'name1', 'order_number' => 2, 'unknow_param': 'hoge' }]
+            'normal' => [{ 'id' => '', 'order_number' => 2 }],
+            'bad' => [
+              { 'id' => '', 'name' => 'name1', 'order_number' => 2, 'unknow_param': 'hoge' }
+            ]
           }
         end
 
         it 'バリデーションメッセージを取得できること' do
-          subject
+          validate
           expect(form.errors.messages[:params]).to eq(['不正なパラメーターが渡された項目があります:good,normal,bad'])
         end
       end
@@ -92,29 +89,34 @@ describe SelfCareClassificationsForm, type: :model do
       context '存在しないIDをパラメーターにした場合' do
         let(:params) do
           {
-            'good' => [{ 'id' => '99999999999999999999', 'name' => 'name1', 'order_number' => 2 }],
-            'normal' => [],
+            'good' => [
+              { 'id' => '99999999999999999999', 'name' => 'name1', 'order_number' => 2 }
+            ],
+            'normal' => [
+              { 'id' => other_user_classification.id, 'name' => 'name1', 'order_number' => 2 }
+            ],
             'bad' => []
           }
         end
+        let(:other_user_classification) { create(:self_care_classification) }
 
         it 'バリデーションメッセージを取得できること' do
-          subject
-          expect(form.errors.messages[:params]).to eq(['不正なIDが渡された項目があります:good'])
+          validate
+          expect(form.errors.messages[:params]).to eq(['不正なIDが渡された項目があります:good,normal'])
         end
       end
     end
   end
 
   describe 'save!' do
-    subject { form.save! }
+    subject(:save!) { form.save! }
 
     context 'パラメーターが正しい場合' do
       context '新規作成の場合' do
         let(:params) do
           {
             'good' => [
-              { 'id' =>  '',  'name' => 'name1',  'order_number' => 1 }
+              { 'id' =>  '',  'name' => 'name1', 'order_number' => 1 }
             ],
 
             'normal' => [
@@ -122,24 +124,23 @@ describe SelfCareClassificationsForm, type: :model do
               { 'id' =>  '',  'name' =>  'name2',  'order_number' =>  2 }
             ],
 
-            'bad' => [
-            ]
+            'bad' => []
           }
         end
 
         it 'パラメーター数作成できること' do
-          subject
+          save!
           expect(SelfCareClassification.where(name: 'name1').count).to eq(2)
         end
 
         it 'グループごとに作成できていること' do
-          subject
+          save!
           # TODO: scopeを設定する
           expect(SelfCareClassification.where(kind: :good).count).to eq(1)
         end
 
         it 'order_numberを正しく設定できていること' do
-          subject
+          save!
           order_numbers = SelfCareClassification.where(kind: :normal).pluck(:order_number)
           expect(order_numbers).to eq([1, 2])
         end
@@ -153,16 +154,22 @@ describe SelfCareClassificationsForm, type: :model do
               { 'id' =>  '', 'name' => 'name2', 'order_number' => '5' },
               { 'id' => good_classification.id, 'name' => 'name2',  'order_number' => '4' }
             ],
-            'normal' => [{ 'id' => normal_classification.id, 'name' => 'name1', 'order_number' => '1' }],
+            'normal' => [
+              { 'id' => normal_classification.id, 'name' => 'name1', 'order_number' => '1' }
+            ],
             'bad' => []
           }
         end
 
-        let(:good_classification) { create(:self_care_classification, user: user, order_number: 5, kind: :good) }
-        let(:normal_classification) { create(:self_care_classification, user: user, order_number: 5, kind: :normal) }
+        let(:good_classification) do
+          create(:self_care_classification, user: user, order_number: 5, kind: :good)
+        end
+        let(:normal_classification) do
+          create(:self_care_classification, user: user, order_number: 5, kind: :normal)
+        end
 
         it 'グループごとに作成できていること' do
-          subject
+          save!
           # TODO: scopeを設定する
           expect(SelfCareClassification.where(kind: :good).count).to eq(3)
         end
@@ -184,11 +191,11 @@ describe SelfCareClassificationsForm, type: :model do
         end
 
         it '例外が発生すること' do
-          expect { subject }.to raise_error(SelfCareClassificationsForm::InvalidError)
+          expect { save! }.to raise_error(SelfCareClassificationsForm::InvalidError)
         end
       end
 
-      pending '同じ項目名で同じorder_numberが存在する場合'''
+      pending '同じ項目名で同じorder_numberが存在する場合'
       pending '保存に1つでも失敗した場合に全件保存されないこと'
     end
   end
